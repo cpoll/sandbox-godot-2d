f655998d999e1ea2dadee5f2c6400fe574997a18
Cristian Poll <git@cristianpoll.com>
2026-01-25 14:29:55 -0500

    add naive bullet spawning

---
+++ experiments/naive_bullet_spawning/naive_bullet_spawning.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var bullet = load("res://experiments/naive_bullet_spawning/bullet.tscn")
+
+const ROWS: int = 50
+const BULLET_FREQUENCY = 0.2
+
+var timer = 0
+var row_width
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    # We'll calculate row width as if there's one extra row so that we can shift the rows a half-step to the right
+    row_width = get_viewport().size.x / (ROWS + 1)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    timer -= delta
+    if timer < 0:
+        spawn_bullets()
+        timer+= BULLET_FREQUENCY
+
+func spawn_bullets() -> void:
+    for row in range(ROWS):
+        var b = bullet.instantiate()
+        add_child(b)
+        b.position.x = row * row_width + row_width
+        
+        
+    
