ea35fe04037fb0ba2c521193a77a2256938cd852
Cristian Poll <git@cristianpoll.com>
2026-02-01 14:23:30 -0500

    test 15: dumb hand-rolled collision

--- experiments/bullet_spawning/bullet_spawning.gd
+++ experiments/bullet_spawning/bullet_spawning.gd
@@ -5,6 +5,7 @@ class_name BulletManager
 var bullet = load("res://experiments/bullet_spawning/dumb_bullet.tscn")
 @onready var bulletcount: Label = $'%Ui/%BulletCount'
 @onready var bullet_container = $'%Bullets'
+@onready var player = $'%Player'
 
 const ROWS: int = 50
 const BULLET_FREQUENCY = 0.1
@@ -27,7 +28,7 @@ func _ready() -> void:
     bullets = []
     bounds = Vector2(screen_size.x - 50, screen_size.y - 50)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
     bulletcount.text = "Bullets: %s\nFPS: %s" % [len(bullets), Engine.get_frames_per_second()]
     pass
 
@@ -39,6 +40,8 @@ func _physics_process(delta: float) -> void:
     if timer < 0:
         spawn_bullets()
         timer+= BULLET_FREQUENCY
+        
+    test_collision()
 
 func spawn_bullets() -> void:
     for row in range(ROWS):
@@ -79,9 +82,21 @@ func despawn_dumb_bullet(b: Node2D):
     
 func move_bullets(delta: float):
     for b in bullets:
-        b.position.y += delta * BULLET_SPEED/4
+        b.position.y += delta * BULLET_SPEED
         if b.position.x > bounds.x or b.position.y > bounds.y:
             if b is Bullet:
                 despawn_bullet(b)
             else:
                 despawn_dumb_bullet(b)
+                
+func test_collision():
+    var bullet_radius_pixels = 12
+    var player_radius_pixels = 100
+    
+    # Optimization: distance_squared_to is cheaper because we don't need to sqrt (an expensive computation)
+    # TODO: Profile this, how much worse is regular distance_to?
+    var collision_distance_squared = (bullet_radius_pixels + player_radius_pixels) ** 2
+    var player_position: Vector2 = player.position
+    for b in bullets:
+        if player_position.distance_squared_to(b.position) < collision_distance_squared:
+            despawn_dumb_bullet(b)
--- experiments/bullet_spawning/player.gd
+++ experiments/bullet_spawning/player.gd
@@ -1,11 +1,8 @@
 extends Sprite2D
 
 func _ready() -> void:
-    get_node(^"Area2D").area_entered.connect(_on_area_entered)
-
-func _process(delta: float) -> void:
     pass
     
-func _on_area_entered(n: Node):
-    if n.owner is Bullet:
-        n.owner.explode()
+func _process(_delta: float) -> void:
+    pass
+    
